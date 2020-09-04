7ebab8c86acab3aab6b78907d77a27faab438841
muzzamilpz <68339671+muzzamilpz@users.noreply.github.com>
2020-09-04 12:11:08 +0530

    Add files via upload

---
+++ Day2 Assignment.js
@@ -0,0 +1,73 @@
+//Hey Guys Its me Muzzamil.
+//here i written my Day 2 Assignment
+//Email: muzzamilpz@gmail.com
+
+//1)Program to Find a Particular Character in a String 
+let name = "Hello LetsUpgrade";
+console.log(name);
+
+//finding a particular string
+
+console.log(name.indexOf("Up"));
+
+
+// //Output is 10
+
+// //##################################/
+// //2)Program to convert minutes to seconds
+
+let time =5;
+
+let sec= time*60;
+
+console.log(`Answer 5 minutes = ${sec} `);
+
+// Output
+
+//Answer 5 minutes = 300 
+
+
+//******************************//
+//3)Program to search element in a array of strings
+
+let array =[10,20,30,40,50,60,70];
+
+let findtheval = array.includes(70);
+console.log(findtheval);
+
+
+//Output
+//True
+
+//4)Program to display only elements containing 'a' in the term of array
+
+let array = ["Apps","Apple","Animals","Fruits","Banana","Cat","Dog"];
+
+if(array="A"){
+     console.log(array);
+ }
+else{
+    console.log("No");
+}
+
+
+//5)Print an array in reverse order
+
+
+Array = ["Animal","Fruits","Vegetables"];
+console.log(Array);
+Array = ["Animal","Fruits","Vegetables"].reverse();
+console.log(Array);
+
+
+
+
+
+
+
+
+
+
+
+
+
